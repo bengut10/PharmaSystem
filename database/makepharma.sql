24f1bebc3d48d68da67b9e396e2ffda6bc0291d1
Jose Gutierrez <j_g572@zeus.cs.txstate.edu>
2016-10-30 17:12:13 -0500

    updated schema

--- database/makepharma.sql
+++ database/makepharma.sql
@@ -1,139 +1,146 @@
 
+
 CREATE TABLE physician
 (
-  drlicense CHAR(255) NOT NULL,
-  name CHAR(255) NOT NULL,
-  phone NUMERIC(12,0) NOT NULL,
-  PRIMARY KEY (drlicense)
-)
-/
+    drLicense CHAR(255),
+    dName CHAR(255) NOT NULL,
+    phone NUMERIC(10,0) NOT NULL UNIQUE,
+    PRIMARY KEY (drLicense)
+);
+
+INSERT INTO physician(drLicense, dName, phone) VALUES
+    ('a1234', 'Roger Smith', 5129672324);
+INSERT INTO physician(drLicense, dName, phone) VALUES
+    ('b3267', 'Frank James', 5129835670);
+INSERT INTO physician(drLicense, dName, phone) VALUES
+    ('g1184', 'Ray Lopez', 5129065324);
+
 
 CREATE TABLE drug
 (
-  compound CHAR(255) NOT NULL,
-  price INT NOT NULL,
-  PRIMARY KEY (compound)
-)
-/
+    dName CHAR(255),
+    price FLOAT NOT NULL,
+    PRIMARY KEY(dName)
+);
+
+INSERT INTO drug(dName, price) VALUES
+    ('adderall', .99);
+INSERT INTO drug(dName, price) VALUES
+    ('ibuprofen', .29);
+INSERT INTO drug(dName, price) VALUES
+    ('codeine', 1.49);
 
 CREATE TABLE diagnostic
 (
-  code NUMERIC(5,2) NOT NULL,
+  code NUMERIC(5,2),
   description CHAR(255) NOT NULL,
   PRIMARY KEY (code)
-)
-/
+);
+
+INSERT INTO diagnostic(code, description) VALUES
+    (434.23, 'severe pain');
+INSERT INTO diagnostic(code, description) VALUES
+    (235.37, 'narcolepsy');
+INSERT INTO diagnostic(code, description) VALUES
+    (342.12, 'mild pain');
 
 CREATE TABLE insurance
 (
-  policy CHAR(255) NOT NULL,
+  policy CHAR(255),
   name CHAR(255) NOT NULL,
   copay FLOAT NOT NULL,
   PRIMARY KEY (policy)
-)
-/
+);
 
 CREATE TABLE supports
 (
   policy CHAR(255) NOT NULL,
   compound CHAR(255) NOT NULL,
   FOREIGN KEY (policy) REFERENCES insurance(policy),
-  FOREIGN KEY (compound) REFERENCES drug(compound)
-)
-/
+  FOREIGN KEY (compound) REFERENCES drug(dName)
+);
+
 
 CREATE TABLE inventory
 (
-  location INT NOT NULL,
+  location INT NOT NULL, --Reffers to a isle number in the store.--
   quantity INT NOT NULL,
   compound CHAR(255) NOT NULL,
-  FOREIGN KEY (compound) REFERENCES drug(compound)
-)
-/
-
-CREATE TABLE medPrescription
-(
-  expiration DATE NOT NULL,
-  rx INT NOT NULL,
-  quantity INT NOT NULL,
-  admin CHAR(255) NOT NULL,
-  indication CHAR(255),
-  refill INT NOT NULL,
-  code NUMERIC(5,2) NOT NULL,
-  compound CHAR(255) NOT NULL,
-  PRIMARY KEY (rx),
-  FOREIGN KEY (code) REFERENCES diagnostic(code),
-  FOREIGN KEY (compound) REFERENCES drug(compound),
-  UNIQUE ()
-)
-/
+  FOREIGN KEY (compound) REFERENCES drug(dName)
+);
 
 CREATE TABLE payment
 (
-  authorizarion INT NOT NULL,
+  authorizarion INT,
   cost FLOAT NOT NULL,
   policy CHAR(255) NOT NULL,
   compound CHAR(255) NOT NULL,
   PRIMARY KEY (authorizarion),
   FOREIGN KEY (policy) REFERENCES insurance(policy),
-  FOREIGN KEY (compound) REFERENCES drug(compound)
-)
-/
-
-CREATE TABLE transaction
-(
-  timestamp DATE NOT NULL,
-  authorizarion INT NOT NULL,
-  FOREIGN KEY (authorizarion) REFERENCES payment(authorizarion)
-)
-/
+  FOREIGN KEY (compound) REFERENCES drug(dName)
+);
 
 CREATE TABLE customer
 (
+  id INT,
   name CHAR(255) NOT NULL,
   dob DATE NOT NULL,
   phone INT NOT NULL,
-  ssn INT NOT NULL,
   address CHAR(255) NOT NULL,
   policy CHAR(255) NOT NULL,
-  PRIMARY KEY (ssn),
+  PRIMARY KEY (id),
   FOREIGN KEY (policy) REFERENCES insurance(policy)
-)
-/
+);
+
+CREATE TABLE prescription
+(
+  rx INT,
+  expiration DATE NOT NULL,
+  quantity INT NOT NULL,
+  admin CHAR(255) NOT NULL,
+  indication CHAR(255), --may be null
+  refill INT NOT NULL,
+  code NUMERIC(5,2) NOT NULL,
+  compound CHAR(255) NOT NULL,
+  physician CHAR(255) NOT NULL,
+  patient int NOT NULL,
+  PRIMARY KEY (rx),
+  FOREIGN KEY (physician) REFERENCES physician(drLicense),
+  FOREIGN KEY (code) REFERENCES diagnostic(code),
+  FOREIGN KEY (compound) REFERENCES drug(dName),
+  FOREIGN KEY (patient) REFERENCES customer(id)
+);
+
+CREATE TABLE sale
+(
+  timestamp DATE NOT NULL,
+  authorizarion INT NOT NULL,
+  rx INT NOT NULL,
+  FOREIGN KEY (rx) REFERENCES prescription(rx),
+  FOREIGN KEY (authorizarion) REFERENCES payment(authorizarion)
+);
 
 CREATE TABLE treats
 (
   compound CHAR(255) NOT NULL,
   code NUMERIC(5,2) NOT NULL,
-  FOREIGN KEY (compound) REFERENCES drug(compound),
+  FOREIGN KEY (compound) REFERENCES drug(dName),
   FOREIGN KEY (code) REFERENCES diagnostic(code)
-)
-/
+);
 
-CREATE TABLE consults
-(
-  ssn INT NOT NULL,
-  drlicense CHAR(255) NOT NULL,
-  FOREIGN KEY (ssn) REFERENCES customer(ssn),
-  FOREIGN KEY (drlicense) REFERENCES physician(drlicense)
-)
-/
-
-CREATE TABLE cPrescription
-(
-  ssn INT NOT NULL,
-  rx INT NOT NULL,
-  FOREIGN KEY (ssn) REFERENCES customer(ssn),
-  FOREIGN KEY (rx) REFERENCES medPrescription(rx)
-)
-/
+INSERT INTO treats (compound, code) VALUES
+    ('adderall', 235.37);
+INSERT INTO treats (compound, code) VALUES
+    ('ibuprofen', 342.12);
+INSERT INTO treats (compound, code) VALUES
+    ('codeine', 434.23);
+    
 
 CREATE TABLE allergy
 (
-  ssn INT NOT NULL,
+  name INT NOT NULL,
   compound CHAR(255) NOT NULL,
-  FOREIGN KEY (ssn) REFERENCES customer(ssn),
-  FOREIGN KEY (compound) REFERENCES drug(compound)
+  FOREIGN KEY (name) REFERENCES customer(id),
+  FOREIGN KEY (compound) REFERENCES drug(dName)
 );
 
-  
---
+++ database/rmpharma.sql
@@ -0,0 +1,13 @@
+
+DROP TABLE allergy;
+DROP TABLE treats;
+DROP TABLE sale;
+DROP TABLE prescription;
+DROP TABLE customer;
+DROP TABLE payment;
+DROP TABLE inventory;
+DROP TABLE supports;
+DROP TABLE drug;
+DROP TABLE diagnostic;
+DROP TABLE insurance;
+DROP TABLE physician;
